38b94d5de73782926b58d58f555ad5de3c1440ea
Napssters <niko.orozc@gmail.com>
2023-07-14 17:57:39 -0500

    se hacen ajustes importantes para la ejecucion

--- src/cocktails/cocktails.module.ts
+++ src/cocktails/cocktails.module.ts
@@ -4,11 +4,23 @@ import { CocktailsController } from './controller/cocktails.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { Cocktails } from 'src/entities/cocktails.entity';
+import { IngredientsModule } from 'src/ingredients/ingredients.module';
+import { PreparedCocktailService } from './services/prepared-cocktail.service';
+import { PreparedCocktails } from 'src/entities/prepared-cocktail.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Cocktails])],
+    imports: [
+        TypeOrmModule.forFeature([Cocktails, PreparedCocktails]),
+        IngredientsModule
+    ],
     controllers: [CocktailsController],
-    providers: [CocktailsService],
-    exports: [CocktailsService],
+    providers: [
+        CocktailsService,
+        PreparedCocktailService
+    ],
+    exports: [
+        CocktailsService,
+        PreparedCocktailService
+    ],
 })
 export class CocktailsModule { }
